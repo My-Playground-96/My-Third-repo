4e6e9359d00e33ad03494eb7194802357fb92321
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-07-29 23:01:03 +0530

    Complete Task 1

--- Demo1.java
+++ Demo1.java
@@ -3,7 +3,6 @@ public class Demo1{
     public static void main(String[] args) {
         
         float bankAcc = 123456.7890f;
-
         System.out.printf("Your Account balance is = %,.2f\n",bankAcc);
         
     }
