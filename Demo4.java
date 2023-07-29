a53afc2f3d440d70ff42f5e683e1add0772477c6
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-07-29 23:51:50 +0530

    Enter Inputs

--- Demo4.java
+++ Demo4.java
@@ -1,3 +1,16 @@
 public class Demo4 {
+
+    public static void main(String[] args) {
+        
+        int date = 28;
+        int mounth = 12;
+        int year = 2023;
+        int hour = 23;
+        int minutes = 59;
+        int seconds = 59;
+        
+
+
+    }
     
 }
