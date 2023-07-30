ff7d7b9aa03cedcf16a8fa261f7e69ce6a8c120b
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-07-30 22:45:11 +0530

    Enter Inputs

--- Demo5.java
+++ Demo5.java
@@ -2,8 +2,12 @@ public class Demo5{
 
     public static void main(String[] args) {
         
-        
+        int prefix = 978;
+        int group = 3;
+        int pulisher = 16;
+        int publication = 1484100;
 
+        
     }
 
 }
