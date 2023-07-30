a3402745601a77d0f32148b811974daee752a284
Tharindu Madushan <tharindumpathirana96@gmail.com>
2023-07-30 23:07:57 +0530

    Complete Task 5

--- Demo5.java
+++ Demo5.java
@@ -6,6 +6,7 @@ public class Demo5{
         int group = 3;
         int pulisher = 16;
         int publication = 1484100;
+        System.out.printf("\033[34m%s\033[0m-\033[33m%s\033[0m-\033[32m%s\033[0m-\033[31m%s\n",prefix,group,pulisher,publication);
 
         
     }
